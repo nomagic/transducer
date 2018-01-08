d538382f2f3567e50bbe4ab04fd629c14954c99c
nomagic <nomagic@users.noreply.github.com>
2018-01-08 13:05:06 -0800

    It's a lot easier to start out with a github repository than to import your own local repository

--- test/com/nomagicsoftware/functional/TransducerTest.java
+++ test/com/nomagicsoftware/functional/TransducerTest.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.nomagicsoftware.functional;
 
 import com.nomagicsoftware.functional.Transducer.Reducer;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.Spliterators;
-import java.util.function.BiFunction;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
